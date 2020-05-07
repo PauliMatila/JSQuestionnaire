2d4fd25d764b8eb951efcc60d8f339df2638f0a9
Pauli Matila <pauli.matila@outlook.com>
2020-05-07 21:03:47 +0300

    jääty askNextQuestion functioon

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-var currentQuestionID = [];
-var answerIndexes = [];
+var currentQuestionID;
+var answerIndexes;
 var questions = [];
 
 function onStart()
@@ -7,22 +7,22 @@ function onStart()
     var Q1 = new Question("Onko koodaaminen helppoa", ["Heleppoa on","Ei ole heleppoa"]);
     var Q2 = new Question("Onko elämä helppoa", ["Erittäin heleppoa","Ei ole heleppoa ollenkaan"]);
     questions.push(Q1, Q2);
-    document.getElementById("questionsBlock").style.visibility="hidden";
-    document.getElementById("endSummary").style.visibility="hidden";
+    document.getElementById("questionsBlock").style.display="none";
+    document.getElementById("endSummary").style.display="none";
 }
 
-function startQuestionaire()
+function startQuestionnaire()
 {
     currentQuestionID = 0;
-    var answers = new answerIndexes();
+    answerIndexes = new Array();
     askNextQuestion();
 }
 
 function askNextQuestion()
 {
-    if (currentQuestionID == 0)
+    if (questions.lenght > currentQuestionID)
     {
-        showQuestion();
+        showQuestion(questions[currentQuestionID]);
     }
     else
     {
@@ -30,16 +30,11 @@ function askNextQuestion()
     }
 }
 
-function showEndSummary()
-{
-    document.getElementById("endBlock").innerHTML = Question(questions, answerIndexes);
-    document.getElementById("startBlock").style.visibility="hidden";
-    document.getElementById("questionsBlock").style.visibility="hidden";
-}
-
 function showQuestion(question)
 {
     document.getElementById("questionline").innerHTML = Question(question);
+    document.getElementById("startBlock").style.display="none";
+    document.getElementById("questionsBlock").style.display="block";
     showAnswerOptions();
 }
 
@@ -47,6 +42,13 @@ function showAnswerOptions(answer)
 {
     document.getElementById("answerBlock").innerHTML = Question(answer);
 
+}
+
+function showEndSummary()
+{
+    document.getElementById("endBlock").innerHTML = Question(questions, answerIndexes);
+    document.getElementById("startBlock").style.visibility="hidden";
+    document.getElementById("questionsBlock").style.visibility="hidden";
 }
 
 function answerButtonClicked(index)
